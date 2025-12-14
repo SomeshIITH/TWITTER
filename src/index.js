4c920f253009699be719f0635fc7905adcbcf056
SomeshIITH <shivjaiswal51230@gmail.com>
2025-12-14 23:10:46 +0530

    added comment and tweet model along with samples in index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const app = express();
 const PORT = 3000
 
 const Tweet = require('./models/tweet')
+const Comment = require('./models/comment')
 
 const startServer = () => {
     app.listen(PORT, async () => {
@@ -14,8 +15,17 @@ const startServer = () => {
         await connect();
         console.log('connection established');
 
-        const tweet = await Tweet.create({content : "Hello world"});
-        console.log('Created tweet', tweet);
+        // const tweet = await Tweet.create({content : "Hello world" , userEmail : "a@b.com"});
+        // const tweet = await Tweet.find({userEmail : "a@b.com"})
+        // const tweet = await Tweet.findById("693e794fc9e3bbc1c3d23e59");
+        // tweet.userEmail = "b@c.com";
+        // await tweet.save();
+        const tweet = await Tweet.create({content : "tweet with comments"});
+        const comment = await Comment.create({content : "comment content for tweet"});
+        tweet.comments.push(comment);//because its array
+        await tweet.save();
+        console.log(tweet);
+
     });
 };
 
--- src/models/tweet.js
+++ src/models/tweet.js
@@ -7,7 +7,10 @@ const tweetSchema = new mongoose.Schema({
     },
     userEmail : {
         type : String
-    }
+    },
+    comments : [{
+        type : mongoose.Schema.Types.ObjectId
+    }]
 },{timestamps : true});//timestamp will add createdAt and updatedAt
 
 const Tweet = mongoose.model('Tweet', tweetSchema);
